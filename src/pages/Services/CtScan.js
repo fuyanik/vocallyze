000e6b2b636be391128604dded9dca76803b71ae
fuyanik <furkan.uyanik@boun.edu.tr>
2023-09-07 01:45:55 +0300

    bug fixed

--- src/pages/Services/CtScan.js
+++ src/pages/Services/CtScan.js
@@ -4,7 +4,7 @@ import Services from './Services'
 const CtScan = () => {
   return (
     <Services
-    title={'Ct Scan'}
+    title={'CT Scan'}
     imageUrl={'https://res.cloudinary.com/scan-com/image/upload/f_webp/q_auto:good/c_scale,w_500/v1/images/services/mri-hero_jpg.jpg' }
     heroText={"We've made it easier than ever to book a private MRI scan near you. By partnering with over 150 scanning centres across the UK, we're able to show you every available option - from prices to locations to services - to help you make the best decision for you."}
     />
