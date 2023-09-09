8f28732ea7c4aa557217eb5cb965270ffce45c0d
fuyanik <furkan.uyanik@boun.edu.tr>
2023-09-10 02:03:11 +0300

    bug fixed

--- src/pages/PayScreen/checkoutForm.js
+++ src/pages/PayScreen/checkoutForm.js
@@ -146,7 +146,7 @@ export default function CheckoutForm() {
           confirmParams: {
               //return_url: "http://localhost:3000/pay-succes"
               //return_url: "https://www.mitrua.com/pay-succes"
-              return_url: "https://mitrua.com/pay-succes2"
+              return_url: "https://google.com/"
           },
         });
     
