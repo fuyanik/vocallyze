7c570850c3a44ea520a66bbbf23e1d70b82493be
fuyanik <furkan.uyanik@boun.edu.tr>
2023-09-10 03:07:42 +0300

    bu gfixed

--- src/pages/PayScreen/checkoutForm.js
+++ src/pages/PayScreen/checkoutForm.js
@@ -146,7 +146,7 @@ export default function CheckoutForm() {
           confirmParams: {
               //return_url: "http://localhost:3000/pay-succes"
               //return_url: "https://www.mitrua.com/pay-succes"
-              return_url:   "https://stupendous-treacle-24cffd.netlify.app/redirect"
+              return_url:   "https://vitamu.com/redirect"
           },
         });
     
