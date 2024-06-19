39b6e8416b35ecb27228194e2d9b94cd2ba9d13b
fuyanik <furkan.uyanik@boun.edu.tr>
2024-06-19 13:14:07 +0300

    bug fixed

--- src/homeComponents/7.Radiologists/radiologists.js
+++ src/homeComponents/7.Radiologists/radiologists.js
@@ -318,7 +318,7 @@ return (
     <div className="doctor-main">
       <div className="text1 hidden"> OUR RADIOLOGY TEAM</div>
       <div className="text2">
-      We are a team of experienced radiologists. We are licenced and registered with the U.K General Medical Council and are members of the Royal College of Radiologists. Two of us will recheck your radiology images to make sure that you always receive the correct diagnosis.
+      We are a team of experienced radiologists. We are a team of U.S. Board certified radiologists.
       </div>
 
       <div className="doctor-section">
